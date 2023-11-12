f42e1dd36075475fc2c056f3a5b44abda7c88b99
Daleock1 <142682523+Daleock1@users.noreply.github.com>
2023-11-12 09:53:19 -0500

    Add files via upload

---
+++ GestionDeDatosPacientes/src/GestionDeDatosPacientes/Doctores.java
@@ -0,0 +1,50 @@
+
+
+public class Doctores {
+    private int numeroColegiatura;
+    private String nombre;
+    private String especialidad;
+    
+    
+    
+    public Doctores(int numeroColegiatura, String nombre, String especialidad) {
+        this.numeroColegiatura = numeroColegiatura;
+        this.nombre = nombre;
+        this.especialidad = especialidad;
+    }
+
+   
+    public int getNumeroColegiatura() {
+        return numeroColegiatura;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+   
+    public void setNumeroColegiatura(int numeroColegiatura) {
+        this.numeroColegiatura = numeroColegiatura;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+
+	@Override
+	public String toString() {
+		return "Doctores [numeroColegiatura=" + numeroColegiatura + ", nombre=" + nombre + ", especialidad="
+				+ especialidad + "]";
+	}
+    
+}
+
